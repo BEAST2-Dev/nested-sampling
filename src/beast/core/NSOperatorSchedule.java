0075e44217a4ca0e991d46e0600d562d705f3205
Remco Bouckaert <higgs.ml@gmail.com>
2018-04-24 17:33:46 +1200

    add input to prevent cycling of operators

--- src/beast/core/NSOperatorSchedule.java
+++ src/beast/core/NSOperatorSchedule.java
@@ -1,6 +1,5 @@
 package beast.core;
 
-import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,12 +8,13 @@ import beast.core.Operator;
 import beast.core.OperatorSchedule;
 import beast.core.StateNode;
 import beast.core.parameter.Parameter;
+import beast.core.util.Log;
 import beast.evolution.tree.TreeInterface;
 import beast.util.Randomizer;
 
 @Description("Operator scedule tuned for NS")
 public class NSOperatorSchedule extends OperatorSchedule {
-	
+	public Input<Boolean> cycleInput = new Input<>("cycle", "adapt operator weights to cycle over all state nodes till every one is changed", true);
 	
 	List<StateNode> stateNodes;
 	double [] stateNodeWeights;
@@ -30,7 +30,7 @@ public class NSOperatorSchedule extends OperatorSchedule {
 	
 	void initialise() {		
     	// never optimise for NS
-    	autoOptimise = false;
+    	autoOptimise = autoOptimiseInput.get();
 
     	// collect stateNodes
 		stateNodes = new ArrayList<>();		
@@ -55,6 +55,9 @@ public class NSOperatorSchedule extends OperatorSchedule {
         	}
         	totalDimension += stateNodeDimensions[i];
         }
+        
+        Log.warning("#parameters = " + totalDimension);
+        
         // initialise stateNodeWeights
         stateNodeWeights = new double[stateNodes.size()];
         System.arraycopy(stateNodeDimensions, 0, stateNodeWeights, 0, stateNodeWeights.length);
@@ -87,8 +90,25 @@ public class NSOperatorSchedule extends OperatorSchedule {
         
         }
 
+        // calculate normalised weights
+        normalizedWeights = new double[operators.size()];
+        for (int i = 0; i < operatorWeights.length; i++) {
+        	for (int j = 0; j < operatorWeights[i].length; j++) {
+        		int k = operators.indexOf(stateNodeOperators[i].get(j));
+        		double w = (j == 0 ? operatorWeights[i][0] : operatorWeights[i][j] - operatorWeights[i][j-1]); 
+        		normalizedWeights[k] += stateNodeDimensions[i] * w;
+        	}
+        }
+        normalizedWeights = Randomizer.getNormalized(normalizedWeights);
         
-    	prevOperator = null;
+        // calculate cumulative operator probs
+        cumulativeProbs = new double[normalizedWeights.length];
+        cumulativeProbs[0] = normalizedWeights[0];
+        for (int i = 1; i < normalizedWeights.length; i++) {
+        	cumulativeProbs[i] = cumulativeProbs[i-1] + normalizedWeights[i];
+        }
+
+        prevOperator = null;
     	prevAcceptedCount = 0;
     	totalAccepted = 0;
     	consecutiveRejectCount = 0;
@@ -99,43 +119,43 @@ public class NSOperatorSchedule extends OperatorSchedule {
     	if (stateNodes == null) {
     		initialise();
     	}
-    	if (prevOperator != null) {
-    		if (prevAcceptedCount != prevOperator.m_nNrAccepted) {
-    			// the prevOperator was accepted, so remove 1 from stateNodeWeights
-    			stateNodeWeights[prevStateNodeIndex]--;
-    			totalAccepted++;
-    			if (totalAccepted == totalDimension) {
-    				// reset
-    				totalAccepted = 0;
-        			consecutiveRejectCount = 0;
-    		        System.arraycopy(stateNodeDimensions, 0, stateNodeWeights, 0, stateNodeWeights.length);
-    			}
-    		} else {
-    			consecutiveRejectCount++;
-    			if (consecutiveRejectCount > totalDimension) {
-    				// reset
-    				totalAccepted = 0;
-        			consecutiveRejectCount = 0;
-    		        System.arraycopy(stateNodeDimensions, 0, stateNodeWeights, 0, stateNodeWeights.length);
-    			}
-    		}
-    	}
     	
-        final int stateNodeIndex = Randomizer.randomChoicePDF(stateNodeWeights);
-        double [] ow = operatorWeights[stateNodeIndex];
-        final int operatorIndex = ow.length == 1 ? 0 : Randomizer.randomChoice(ow);
-        prevOperator = stateNodeOperators[stateNodeIndex].get(operatorIndex);
-        prevAcceptedCount = prevOperator.m_nNrAccepted;
-        prevStateNodeIndex = stateNodeIndex;
-        return prevOperator;
+    	
+    	if (cycleInput.get()) {
+	    	if (prevOperator != null) {
+	    		if (prevAcceptedCount != prevOperator.m_nNrAccepted) {
+	    			// the prevOperator was accepted, so remove 1 from stateNodeWeights
+	    			stateNodeWeights[prevStateNodeIndex]--;
+	    			totalAccepted++;
+	    			if (totalAccepted == totalDimension) {
+	    				// reset
+	    				totalAccepted = 0;
+	        			consecutiveRejectCount = 0;
+	    		        System.arraycopy(stateNodeDimensions, 0, stateNodeWeights, 0, stateNodeWeights.length);
+	    			}
+	    		} else {
+	    			consecutiveRejectCount++;
+	    			if (consecutiveRejectCount > totalDimension) {
+	    				// reset
+	    				totalAccepted = 0;
+	        			consecutiveRejectCount = 0;
+	    		        System.arraycopy(stateNodeDimensions, 0, stateNodeWeights, 0, stateNodeWeights.length);
+	    			}
+	    		}
+	    	}
+	    	
+	        final int stateNodeIndex = Randomizer.randomChoicePDF(stateNodeWeights);
+	        double [] ow = operatorWeights[stateNodeIndex];
+	        final int operatorIndex = ow.length == 1 ? 0 : Randomizer.randomChoice(ow);
+	        prevOperator = stateNodeOperators[stateNodeIndex].get(operatorIndex);
+	        prevAcceptedCount = prevOperator.m_nNrAccepted;
+	        prevStateNodeIndex = stateNodeIndex;
+	        return prevOperator;
+    	} else {
+    		// don't cycle, just pick an operator with static weigth distribution
+            final int operatorIndex = Randomizer.randomChoicePDF(cumulativeProbs);
+            return operators.get(operatorIndex);
+    	}
     }
-
-
-    @Override
-    public void showOperatorRates(PrintStream out) {
-    	out.println("NSOperatorSchedule::showOperatorRates(); not implemented yet");
-    }
-    
-    
     
 }
