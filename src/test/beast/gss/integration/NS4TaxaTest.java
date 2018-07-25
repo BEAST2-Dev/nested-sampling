b600a0b24d9503960e3206fa47fb890ac4508f96
Remco Bouckaert <higgs.ml@gmail.com>
2018-07-25 16:58:03 +1200

    add 4taxa test

---
+++ src/test/beast/gss/integration/NS4TaxaTest.java
@@ -0,0 +1,42 @@
+package test.beast.gss.integration;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import beast.core.Logger;
+import beast.gss.NS;
+import beast.util.Randomizer;
+import beast.util.XMLParser;
+import beast.util.XMLParserException;
+import junit.framework.TestCase;
+
+public class NS4TaxaTest extends TestCase {
+	
+	@Test
+	public void testNS4Taxa() throws SAXException, IOException, ParserConfigurationException, XMLParserException {
+        Logger.FILE_MODE = Logger.LogFileMode.overwrite;
+
+        int seed = 127;
+        String fileName = "examples/NS_4taxa_NormalBirthRate.xml";
+        Randomizer.setSeed(seed);
+        System.out.println("Processing " + fileName);
+        XMLParser parser = new XMLParser();
+        beast.core.Runnable runable = parser.parseFile(new File(fileName));
+        if (runable instanceof NS) {
+            NS ns = (NS) runable;
+            ns.run();
+            double Z = ns.getEvidence();
+            assertEquals(Z, -2349.5333124902536, 1.0);
+        }
+        System.out.println("Done " + fileName);
+	}
+
+}
